d0b3160357bdf9e0c92bde64808641367ee07801
malikArqoup <147098295+malikArqoup@users.noreply.github.com>
2026-08-24 12:56:35 +0300

    Complete task 1 health API and environment settings

---
+++ api/views.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class HealthView(APIView):
+    def get(self, request):
+        return Response({
+            "status": "ok",
+            "version": "1.0.0"
+        })
