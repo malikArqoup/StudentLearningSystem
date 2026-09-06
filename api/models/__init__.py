7eeba469c4cc096a0d62e97095ca2287906f04f3
malikArqoup <147098295+malikArqoup@users.noreply.github.com>
2026-09-06 11:28:00 +0300

    Add enrollment model

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,9 +1,11 @@
 from .user import User, CustomUserManager
 from .course import Course, Resource
+from .enrollment import Enrollment
 
 __all__ = [
     "User",
     "CustomUserManager",
     "Course",
     "Resource",
+    "Enrollment",
 ]
